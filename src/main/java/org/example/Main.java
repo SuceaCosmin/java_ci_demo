47098774a181079fc6d13329d7a2e99a70ab13ff
Cosmin.S <suceacosmin@yahoo.com>
2024-12-13 00:15:40 +0200

    build: added log4j and BuildConfig for version information

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,23 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.java_ci_demo.BuildConfig;
+
 import java.util.ArrayList;
 import java.util.List;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+
+    private static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+
+        logger.info("Running " + BuildConfig.APP_NAME + " version " + BuildConfig.APP_VERSION + "..");
 
         List<Person> list = new ArrayList<>();
         list.add(new Person("Mathew", "Adam", 20));
@@ -14,7 +25,7 @@ public class Main {
         list.add(new Person("Andrew", "Thomson", 16));
 
         for (Person person : list) {
-            System.out.println(person);
+            System.out.println(String.format("Hey there, My name is %s %s and I'm %s years old!", person.getFirstName(), person.getLastName(), person.getAge()));
         }
 
     }
