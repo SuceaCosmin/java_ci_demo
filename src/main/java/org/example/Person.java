d1db3066d58c39fcffcd106619f57de299bf2f42
Cosmin.S <suceacosmin@yahoo.com>
2024-12-12 22:20:07 +0200

    feat: added Person as data model

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isAdult() {
+        return age >= 18;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
